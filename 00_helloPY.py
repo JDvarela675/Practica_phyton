850aa4792fbc6e840c0d4e47ac2c1dc86a263280
JDvarela675 <jdvarelas@unah.hn>
2024-12-14 00:48:03 -0600

    practica_inicio

---
+++ 00_helloPY.py
@@ -0,0 +1,18 @@
+print ("Hola Mundo")
+
+
+#tipos de datos en phyton 
+
+"""
+este es un comentario en varias
+lineas :)
+"""
+
+#1 string 
+print(type("Hola Mundo"))
+#2 Enteros
+print(type(2))
+# tipo complex 
+print(type(3+1j))
+#tipo booleano
+print(type(True))
---
+++ 01_variables.py
@@ -0,0 +1,21 @@
+#Varibles
+
+my_variable_string= 'My string Variable :)'
+print('Variable con valor string:',my_variable_string)
+
+my_variable_int=10
+print('Misma variable pero con valor entero:',my_variable_int)
+
+my_variable_bool=False
+print('Misma variable pero valor booleano:',my_variable_bool)
+
+print(type(my_variable_bool))
+print('este es el valor de :',False)
+
+# transformando variable a un string con la funcion str
+my_variable_to_string=str(my_variable_bool)
+print(type(my_variable_to_string))
+
+#funcion len sirve para medir el tamaño de un string
+print(len(my_variable_to_string))
+
