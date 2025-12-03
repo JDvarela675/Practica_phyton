f3af6937ae400ce002f20a5181bc7299565bc65e
JDvarela675 <jdvarelas@unah.hn>
2025-12-03 15:45:34 -0600

    fechas :3

---
+++ nivel_basico/edad.py
@@ -0,0 +1,14 @@
+edad =int(input("ingrese su edad:"))
+
+if edad <= 17:
+    print("eres menor de edad")
+
+elif  18 <= edad <= 30:
+    print("eres adulto joven")
+
+
+else :
+ print("eres mayor de edad")
+
+
+
---
+++ nivel_intermedio/00_dates.py
@@ -0,0 +1,8 @@
+# importamos la librería datetime
+from datetime import datetime
+
+#creamos una instance de la clase datetime
+now = datetime.now()
+
+print ("la fecha actual es :",now)
+print("estamos en el año :", now.year)
